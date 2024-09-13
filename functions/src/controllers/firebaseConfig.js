7ad18167deb0c872acb162334788b5543027664b
kardeni <karlabenitez851@gmail.com>
2024-09-12 21:16:29 -0600

    Deleting passwords

--- functions/src/controllers/firebaseConfig.js
+++ functions/src/controllers/firebaseConfig.js
@@ -6,12 +6,12 @@ const { getAuth, } = require('firebase/auth');
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBEuwhND_8BnNMGfbFCwy_-CJ2oCETwH0Y",
-    authDomain: "proyecto-terminal-896bb.firebaseapp.com",
-    projectId: "proyecto-terminal-896bb",
-    storageBucket: "proyecto-terminal-896bb.appspot.com",
-    messagingSenderId: "785416266190",
-    appId: "1:785416266190:web:06a839663f1a7e52134b9f"
+    apiKey: "your api key",
+    authDomain: "your domain",
+    projectId: "your proyect id",
+    storageBucket: "your storage bucket",
+    messagingSenderId: "your sender id",
+    appId: "your app id"
   };
   
   
--- functions/src/models/user.model.js
+++ functions/src/models/user.model.js
@@ -6,13 +6,13 @@ const app = express();
 
 
 var config =
-{//var conn = mysql.createConnection({host: {your_host}, user: {username@servername}, password: {your_password}, database: {your_database}, Port: {your_port}[, ssl:{ca:fs.readFileSync({ca-cert filename})}}]);
-    host:'huertalia.mysql.database.azure.com',
-    user:'huertalia',
-    password:'Proyecto5$',
-    database:'huertalia',
-    port: 3306,
-    ssl: {rejectUnauthorized: true, ca: fs.readFileSync("src/DigiCertGlobalRootCA.crt.pem")},
+{
+    host: 'your_host', 
+    user: 'username@servername', 
+    password: 'your_password', 
+    database: 'your_database', 
+    Port: 'your_port', 
+    ssl:{ca:fs.readFileSync('ca-cert filename')},
     connectTimeout: 30000
 };
 
